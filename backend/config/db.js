044868b2f55733556601ba036f90c8e251c04afc
Jatin-Rai <jatinrai258@gmail.com>
2023-10-20 15:09:06 +0530

    connection changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,31 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-        process.exit(1);
+    const connect = async () => {
+        try {
+            const conn = await mongoose.connect(process.env.MONGO_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+            });
+
+            console.log(`MongoDB Connected: ${conn.connection.host}`);
+        } catch (error) {
+            console.error(`Error connecting to MongoDB: ${error.message}`);
+            // Implement retry or error handling logic here
+        }
+    };
+
+    // Retry connection in case of failures
+    const retryDelayMs = 5000; // 5 seconds
+    while (true) {
+        try {
+            await connect();
+            break;
+        } catch (error) {
+            console.error(`Retry in ${retryDelayMs / 1000} seconds...`);
+            await new Promise((resolve) => setTimeout(resolve, retryDelayMs));
+        }
     }
 };
 
